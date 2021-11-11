3022110034b6e5391d973bf42583ec763cc22c2c
HiroUrata <1712026@i.softbank.jp>
2021-11-11 19:27:59 +0900

    RealmCRUD  readFavoriteVideoData 追加

--- RemakeYoutubeAPILesson/Model/Realm/RealmCRUD.swift
+++ RemakeYoutubeAPILesson/Model/Realm/RealmCRUD.swift
@@ -11,7 +11,7 @@ class RealmCRUD{
     
     private let alert = Alert()
     
-    private var realmFavoriteVideoDetailArray:[[String:String]] = []
+    private var realmFavoriteVideoDetailArray:[[String:String?]] = []
 }
 
 extension RealmCRUD{
@@ -44,4 +44,30 @@ extension RealmCRUD{
 }
 
 
+extension RealmCRUD{
+    
+    public func readFavoriteVideoData(targetView:UIViewController,completion: ([[String:String?]]?,Error?) -> Void){
+        
+        do{
+            
+            let realm = try Realm()
+            
+            realmFavoriteVideoDetailArray = []
+            
+            realm.objects(RealmFavoriteVideoDatas.self).forEach { response in
+                
+                realmFavoriteVideoDetailArray.append(["realmThumbnailImageURL":response.realmThumbnailImageURL,
+                                                      "realmTitle":response.realmTitle,
+                                                      "realmChannelTitle":response.realmChannelTitle,
+                                                      "realmVideoPlayerContents":response.realmVideoPlayerContents,
+                                                      "realmDescription":response.realmDescription])
+            }
+            completion(realmFavoriteVideoDetailArray, nil)
+            
+        }catch{
+            completion(nil, error)
+            alert.warningAlert(alertContent: "データの取得に失敗しました。", targetView: targetView)
+        }
+    }
+}
 
